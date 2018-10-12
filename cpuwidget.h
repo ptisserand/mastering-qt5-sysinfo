c45ecd8a270d25a0c6e87d6a3a084324a8e4b95b
Patrice Tisserand <p.tisserand@gmail.com>
2018-10-12 13:42:56 +0200

    Added skeleton for widget

---
+++ cpuwidget.h
@@ -0,0 +1,12 @@
+#ifndef CPUWIDGET_H
+#define CPUWIDGET_H
+
+#include <QWidget>
+
+class CpuWidget : public SysInfoWidget
+{
+public:
+    CpuWidget();
+};
+
+#endif // CPUWIDGET_H
---
+++ sysinfowidget.h
@@ -0,0 +1,17 @@
+#ifndef SYSINFOWIDGET_H
+#define SYSINFOWIDGET_H
+
+#include <QWidget>
+
+class SysInfoWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit SysInfoWidget(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // SYSINFOWIDGET_H
