2855ff3c4dea267f0fa2f4b76d828eaa05c689e4
Patrice Tisserand <p.tisserand@gmail.com>
2018-10-12 12:14:49 +0200

    Switch SysInfo to singleton

--- sysinfo.cpp
+++ sysinfo.cpp
@@ -1,4 +1,21 @@
+#include <QtGlobal>
 #include "sysinfo.h"
+
+#ifdef Q_OS_WIN
+    #include "sysinfowindowsimpl.h"
+#elif defined(Q_OS_LINUX)
+    #include "sysinfolinuximpl.h"
+#endif
+
+SysInfo& SysInfo::instance()
+{
+#ifdef Q_OS_WIN
+    static SysInfoWindowsImpl singleton;
+#elif defined(Q_OS_LINUX)
+    static SysInfoLinuxImpl singleton;
+#endif
+    return singleton;
+}
 
 SysInfo::SysInfo()
 {
