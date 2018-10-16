866f3f069fa398921fb96c944b24e3e864a6d15e
Patrice Tisserand <p.tisserand@softnautic.com>
2018-10-16 16:33:06 +0200

    Added windows implementation for SysInfo

--- sysinfowindowsimpl.h
+++ sysinfowindowsimpl.h
@@ -1,11 +1,26 @@
 #ifndef SYSINFOWINDOWSIMPL_H
 #define SYSINFOWINDOWSIMPL_H
+#include <QtGlobal>
+#include <QVector>
 #include "sysinfo.h"
+
+typedef struct _FILETIME FILETIME;
 
 class SysInfoWindowsImpl : public SysInfo
 {
 public:
     SysInfoWindowsImpl();
+
+	void init() override;
+	double cpuLoadAverage() override;
+	double memoryUsed() override;
+
+private:
+	QVector<qulonglong> cpuRawData();
+	qulonglong convertFileTime(const FILETIME& filetime) const;
+
+private:
+	QVector<qulonglong> mCpuLoadLastValues;
 };
 
 #endif // SYSINFOWINDOWSIMPL_H
